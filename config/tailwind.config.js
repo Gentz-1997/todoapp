6cb82549b276e713b943e44cd5f44d646e306c13
Genki Matsumi <genki.kuromu@gmail.com>
2024-09-05 19:12:00 +0900

    ヘッダー改善

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,4 +19,25 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
   ]
+  ,theme: {
+    extend: {
+        animation: {
+            "tracking-in-expand": "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000)   both"
+        },
+        keyframes: {
+            "tracking-in-expand": {
+                "0%": {
+                    "letter-spacing": "-.5em",
+                    opacity: "0"
+                },
+                "40%": {
+                    opacity: ".6"
+                },
+                to: {
+                    opacity: "1"
+                }
+            }
+        }
+    }
+}
 }

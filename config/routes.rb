8305dad73b07f771788eeea3838f7b5f262bcff0
Genki Matsumi <genki.kuromu@gmail.com>
2024-09-05 21:34:34 +0900

    top画面改善、新規todoページへのリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'todos/new'
+  root 'home#top'
   get 'home/top'
   resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
